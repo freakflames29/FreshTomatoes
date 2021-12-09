49f088c298a7eaa4e3ddc8041b30d76eaa0ba672
Sourav Das <iamsouravdaswade420@gmail.com>
2021-12-09 19:11:32 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-#Fresh Tomatoes
-Rottentomatoes movie rating package
+## Fresh Tomatoes
+It is a python package to get a movie's rotten tomatoes scores.
+## Syntax
+```python
+import freshtomatoes as ft
+movie=ft.FreshTomatoes()
+movie.Scores("MOVIE_NAME")
+name=movie.Scores("MOVIE_NAME",MOVIE_RELEASE_YEAR)
+```
+## Useage
+```python
+import freshtomatoes as ft
+movie=ft.FreshTomatoes()
+movie.Scores("aladdin")
+name=movie.Scores("coco",2017)
+```
+## Output
+`{'audiencescore': '94', 'tomatometerscore': '57'}
+`
+## Installation
+```python
+pip install freshtomatoes
+```
+
+## License
+The package is available as open source under the terms of the MIT License.
--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setuptools.setup(
  name='freshtomatoes',
- version='0.3',
+ version='0.2.1',
  author="Sourav Das",
  author_email="sourav292922@gmail.com",
  description="freshtomatoes will provide rottentomatoes scores of a given movie",
+ long_description=long_description,
+ long_description_content_type="text/markdown",
+ url="https://github.com/freakflames29/FreshTomatoes",
+  project_urls={
+        "Bug Tracker": "https://github.com/freakflames29/FreshTomatoes",
+    },
  packages=setuptools.find_packages(),
  classifiers=[
  "Programming Language :: Python :: 3",
